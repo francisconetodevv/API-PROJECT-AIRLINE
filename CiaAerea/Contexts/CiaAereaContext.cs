448e68e84a6abea96689afa14b272f44fc58e3af
Francisco <franciscoocontatoo@gmail.com>
2025-07-16 00:01:19 -0300

    Configurando a string de conexão com o banco de dados

--- CiaAerea/Contexts/CiaAereaContext.cs
+++ CiaAerea/Contexts/CiaAereaContext.cs
@@ -6,12 +6,25 @@ namespace CIAArea.Contexts;
 // Hernando de DbContext - Classe do Entity Framework Core
 public class CiaAereaContext : DbContext
 {
-    
+
+    // Classe de aspnet que carrega a string de conexão do arquivo json appsettings.json
+    private readonly IConfiguration _configuration;
+
+    public CiaAereaContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     // Representam a tabela do banco
     public DbSet<Aeronave> Aeronaves => Set<Aeronave>();
-    public DbSet<Piloto> Pilotos => Set <Piloto>();
-    public DbSet<Voo> Voos => Set <Voo>();
-    public DbSet<Manutencao> Manutencoes => Set <Manutencao>();
-    public DbSet<Cancelamento> Cancelamentos => Set <Cancelamento>();
+    public DbSet<Piloto> Pilotos => Set<Piloto>();
+    public DbSet<Voo> Voos => Set<Voo>();
+    public DbSet<Manutencao> Manutencoes => Set<Manutencao>();
+    public DbSet<Cancelamento> Cancelamentos => Set<Cancelamento>();
 
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("CiaAerea"));
+    }
 }
